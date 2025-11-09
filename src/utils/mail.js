869099d6ab7165932760ce4aa0aa177e1c99bee7
ProjectManagement <ankitrajak2954@gamil.com>
2025-11-09 18:16:58 +0530

    how to send emials in nodejs

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,4 +1,45 @@
 import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
+
+const sendEmail = async (options) => {
+   const mailGenerator = new Mailgen({
+        theme: "default",
+        product: {
+            name: "Task Manager" , 
+            link: "https://taskmanagerlink.com"
+        }
+    })
+
+   const emailTextual = mailGenerator.generatePlaintext(options.mailgenContent)
+
+   const emailHtml = mailGenerator.generate(options.mailgenContent)
+
+   const transporter = nodemailer.createTransport({
+        host: process.env.MAILTRAP_SMTP_HOST,
+        port: process.env.MAILTRAP_SMTP_PORT,
+        auth: {
+            user: process.env.MAILTRAP_SMTP_USER,
+            pass: process.env.MAILTRAP_SMTP_PASS
+        }
+    })
+
+    const mail = {
+        from: "mail.taskmanager@example.com",
+        to: options.email,
+        subject: options.subject,
+        text: emailTextual,
+        html: emailHtml,
+    }
+
+    try {
+        await transporter.sendMail(mail)
+    } catch (error) {
+        console.error(
+            "Email service failed siliently. Make sure that you have provided your MAILTRAP credentials in the .env file ",
+        );
+        console.error("Error: ",error)
+    }
+};
 
 
 
@@ -43,5 +84,6 @@ const forgotPasswordMailgenContent = (username, PasswordResetUrl) => {
 
 export{
     emailVerificationMailgenContent,
-    forgotPasswordMailgenContent
-}
+    forgotPasswordMailgenContent, 
+    sendEmail,
+};
